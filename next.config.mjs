f306422a9cdc28078f2c776b0155533bce24fee6
Kongkon Jowarder <kongkon4545@gmail.com>
2025-04-18 16:31:31 +0600

    navbar get api integration and loading, error handler create

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["dynamic3zabeer.scaleupdevagency.com"],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/shared/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='h-[400px] w-full flex items-center justify-center'>
+            <h2 className='text-2xl text-black font-bold '>Data is Loading ....</h2>
+        </div>
+    );
+};
+
+export default Loading;
